409c17f7333210f67ba705c24667e7e85f8d0318
JUGTAR SINGH <mail.jugtarsingh@gmail.com>
2025-03-24 14:43:33 +0530

    first commit

---
+++ models/listing.js
@@ -0,0 +1,34 @@
+const mongoose =require("mongoose");
+const review = require("./review");
+
+const listingSchema=new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    description:String,
+    image:{
+
+        url:{
+            type:String,
+            default:"https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        },
+
+        filename:{type: String,
+        default: "listingimage"},
+        
+    },
+    price:Number,
+    location:String,
+    country:String,
+    reviews:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"Review"
+        }
+
+    ]
+})
+
+const Listing= mongoose.model("Listing",listingSchema);
+module.exports=Listing;
---
+++ models/review.js
@@ -0,0 +1,18 @@
+const { number, required } = require("joi");
+const mongoose =require("mongoose");
+
+const reviewSchema= new mongoose.Schema({
+    comment:{type: String,
+         },
+    rating:{
+        type :Number,
+        min:1,
+        max:5
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now(),
+    }
+})
+
+module.exports=mongoose.model("Review",reviewSchema);
